34a3773bde298f6e770efce6c8d6d6e070aa0355
giancarlorhodes <giancarlo.rhodes@gmail.com>
2021-06-04 10:58:14 -0500

    working on keys file

--- UnitTestEncryptDecrypt/UnitTestFileWorker.cs
+++ UnitTestEncryptDecrypt/UnitTestFileWorker.cs
@@ -15,11 +15,11 @@ namespace UnitTestEncryptDecrypt
         private string _testFileName = @"D:\GitHub\giancarlorhodes\EncryptDecrypt\files\testing.txt";
         private string _destFileName = @"D:\GitHub\giancarlorhodes\EncryptDecrypt\files\destination.txt";
 
-
         private string _testCSharpFilName = @"D:\GitHub\giancarlorhodes\EncryptDecrypt\files\testing.cs";
         private string _encryptedCSharpFileName = @"D:\GitHub\giancarlorhodes\EncryptDecrypt\files\encryptedtesting.cs";
         private string _decryptedCSharpFileName = @"D:\GitHub\giancarlorhodes\EncryptDecrypt\files\decryptedtesting.cs";
 
+        private string _keysFileName = @"D:\GitHub\giancarlorhodes\EncryptDecrypt\files\keys.txt";
 
         public UnitTestFileWorker()
         {
@@ -39,6 +39,7 @@ namespace UnitTestEncryptDecrypt
             // assert
             Assert.IsTrue(expected.Length > 0);
         }
+
 
         [TestMethod]
         public void file_to_string_to_file_comparing_lengths()
@@ -78,17 +79,42 @@ namespace UnitTestEncryptDecrypt
         }
 
 
-        //    [TestMethod]
-        //public void not_null_private_key_and_public_vector()
-        //{
-        //    Console.WriteLine("Init Vector: " + _worker.PublicVector);
-        //    Console.WriteLine("Private Key: " + _worker.PrivateKey);
+        [TestMethod]
+        public void store_the_keys_in_a_file()
+        {
+            // Console.WriteLine("Init Vector: " + _worker.PublicVector);
+            // Console.WriteLine("Private Key: " + _worker.PrivateKey);
 
-        //    Assert.IsTrue(_worker.PublicVector != null && _worker.PrivateKey != null);
+            // arrange
+            string _p = "password";
+            Guid guid = Guid.NewGuid();
+            string _salt = Guid.NewGuid().ToString();
+            Crypto c = new Crypto(_p, _salt);
+            string s = "some random content";
+            
 
-        //}
+            // act
+            string e = c.EncryptStringAES(s, null); // need to call this to create keys
+            _worker.DeleteFile(_keysFileName);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Password: " + _p);
+            sb.AppendLine("Salt: " + _salt);
+            sb.AppendLine("Init Vector: " + c.PublicVector);
+            sb.AppendLine("Private Key: " + c.PrivateKey);
+           
+            _worker.StringToFile(sb.ToString(), _keysFileName);
 
 
-        
+            // assert
+            Console.WriteLine("Password: " + _p);
+            Console.WriteLine("Salt: " + _salt);
+            Console.WriteLine("Init Vector: " + c.PublicVector);
+            Console.WriteLine("Private Key: " + c.PrivateKey);
+
+        }
+
+
+
     }
 }
